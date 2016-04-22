aca3573b2a82fba0bf6cb1d5e09e1a84fd66ca19
richard <richardwangwang@gmail.com>
2016-04-22 09:16:18 +0800

    Update requirements.txt.

---
+++ django_excel_to_model/admin.py
@@ -0,0 +1,5 @@
+import models
+from djangoautoconf.auto_conf_admin_tools.admin_register import AdminRegister
+factory = AdminRegister()
+
+factory.register_all_model(models)
--- django_excel_to_model/default_settings.py
+++ django_excel_to_model/default_settings.py
@@ -3,6 +3,7 @@ INSTALLED_APPS +=(
     'bootstrapform',
     'django.contrib.staticfiles',
     'django_tables2',
+    'background_task',
     'django_tables2_reports',
     'django_excel_to_model',
 )
