1e1207fc6c0cbaec358f81ed34a3fbb76a666188
Richard <richard.j.wang@nokia-sbell.com>
2021-03-15 15:13:40 +0800

    Fix issues in xlsb reader.

--- django_excel_to_model/xlsb_reader.py
+++ django_excel_to_model/xlsb_reader.py
@@ -6,10 +6,10 @@ class XlsbSheet(BaseSheet):
     def __init__(self, workbook, index=0):
         self.workbook = workbook
         self.sheet = workbook.get_sheet(index+1)
-        self.title_columns = None
+        self._headers = None
 
     def set_header_row(self, header_row_start_from_0):
-        self.title_columns = self.get_columns(header_row_start_from_0)
+        self._headers = self.get_columns(header_row_start_from_0)
 
     def _format_number(self, cell):
         if cell.v == int(cell.v):
@@ -33,8 +33,8 @@ class XlsbSheet(BaseSheet):
             for cell in row:
                 if cell.v is not None and cell.r < start_row_numbered_from_0:
                     break
-                if cell.v is not None and cell.v not in self.title_columns:
-                    src_key = unicode(self.title_columns[cell.c])
+                if cell.v is not None and cell.v not in self._headers:
+                    src_key = str(self._headers[cell.c])
                     target_key = get_db_field(mapping, src_key)
                     if target_key:
                         res[target_key] = self.parse_cell_value(cell)
@@ -49,5 +49,6 @@ class XlsbSheet(BaseSheet):
         return res
 
     def get_total_rows(self):
-        return len(self.sheet.rows)
+        # no quick total row supported, so use a dummy one
+        return 999999999
 
